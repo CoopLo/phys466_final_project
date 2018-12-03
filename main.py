b1fb651c3a994d5551d4daef0bc4a00ae944010b
CoopLo <lorsung2@illinois.edu>
2018-12-03 12:37:16 -0600

    added comments to skeleton

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import numpy as np
+import scipy
 from matplotlib import pyplot as plt
 
 
+###
+# parameters:
+#    num_atoms: number of atoms we're simulating with. Should be cubic number
+#    beta: temperature parameter, 1/T
+#
+# returns:
+#    lattice with energy on sites distributed according to BE distribution
+###
 def initialize_lattice(num_atoms, beta):
     lattice = np.empty(num_atoms)
 
     # Initialize each site with energy such that average
     # is given temperature
     
+    
     # Take into account energy levels and degeneracy for this?
     return lattice
 
+
+# total energy of lattice
 def energy(lattice):
     pass
+
+
+###
+# parameters:
+#    lattice: the lattice we're working with
+#    site: the site on the lattice being changed
+#    e_coord: the energy coordinate being changed
+#    pm: energy +- 1
+#
+# returns:
+#    Difference in energy from changing specifiec site
+###
+def energy_diff(lattice, site, e_coord, pm):
+    return 1/2 * (((lattice[site]))**2 - (np.sum(lattice[site] + pm))**2)
 
 if __name__ == '__main__':
 
@@ -25,3 +51,4 @@ if __name__ == '__main__':
     lattice = initialize_lattice(num_atoms, beta)
 
     pass
+
