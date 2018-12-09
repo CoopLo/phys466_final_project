6aac65ba5b0c8e0651c58f0e4522cd40b0b1c4b9
glu2018 <33524052+glu2018@users.noreply.github.com>
2018-12-09 17:58:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def initialize_lattice(num_atoms, beta, lim):
       NOTE: Assumes energy levels of each particle same as that of quantum harmonic oscillator hbar * omega (n + 1/2)         
       '''
     
-    nx = int(num_atoms ** 1/3)
+    nx = int(round(num_atoms ** 1/3))
     lattice = np.ones([nx, nx, nx, 3], dtype=float)
     lat = np.random.rand(nx, nx, nx)
     prob = []
@@ -54,14 +54,32 @@ if __name__ == '__main__':
     # Initial conditions in reduced units
     beta = 1.
     mass = 48.
-    num_atoms = 10
+    lim = 10
+    num_atoms = 100
+    nsweep = 1000
+    lat_dat = 'lat.dat'
+    scalar_dat = 'scalar.dat'
+    nconf = 10
+    ntherm = 1
+    therm_fmt = '%8d %14.8f %14.8f %14.8f\n'
+    therm_header = '# isweep  m2  mavg  pe\n'
 
     # Initialize the lattice at specified temperature
-    lattice = initialize_lattice(num_atoms, beta)
-
-    pass
-<<<<<<< HEAD
-=======
-
->>>>>>> f33e2ac83c637736bab553c818dcf4504af7feb9
+    lattice = initialize_lattice(num_atoms, beta, lim)
+    
+    restart = False
+    if restart:
+        lat = np.loadtxt('last_lat.dat')
+        nx = lat.shape[0]
+    else:
+        with open(lat_dat, 'w') as flat:
+            flat.write('# nx=%d\n' % nx)
+        with open(scalar_dat, 'w') as fsca:
+            fsca.write(therm_header)
+    flat = open(lat_dat, 'a')
+    fsca = open(scalar_dat, 'a')
+    
+    msg = 'starting %s sweeps with %d 
+    if restart:
+           msg = 're'+msg
 
